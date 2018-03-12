e9e74cc43e56959fc7f35d91a346c00f322d7dbb
Sergey <memonanofsa@gmail.com>
2018-03-12 21:38:18 +0500

    ICEX-736 #comment: change meta for init project

--- meta.js
+++ meta.js
@@ -8,18 +8,8 @@ module.exports = {
     name: {
       'type': 'string',
       'required': true,
-      'message': 'Project name'
-    },
-    description: {
-      'type': 'string',
-      'required': false,
-      'message': 'Project description',
-      'default': 'Nuxt.js project'
-    },
-    author: {
-      'type': 'string',
-      'message': 'Author'
-    },
+      'message': 'Landing name'
+    }
   },
   completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
 };
